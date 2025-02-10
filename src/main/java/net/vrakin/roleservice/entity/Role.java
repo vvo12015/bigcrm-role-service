ecc51052d9028dd8ff4640f56fb9fc70f4b4f5f0
Валентин Вракін <vvo12015@gmail.com>
2025-02-10 11:58:53 +0200

    Initial commit

---
+++ src/main/java/net/vrakin/roleservice/entity/Role.java
@@ -0,0 +1,18 @@
+package net.vrakin.roleservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Column(nullable = false, unique = true)
+    private String name;
+}
